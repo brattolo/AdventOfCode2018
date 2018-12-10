0cd0790dc63a51720f8181a837ae2eb55a91f565
Bart Gevaert <bart.gevaert@outlook.com>
2018-12-10 22:54:27 +0100

    Day1

---
+++ Day1/Program.fs
@@ -0,0 +1,33 @@
+﻿// Learn more about F# at http://fsharp.org
+module Day1Program
+
+open System
+open System.IO
+open System.Linq
+
+let readLines (filePath: string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+       yield sr.ReadLine()
+}
+
+let frequencyChanges = readLines "input.txt" |> Seq.map Int32.Parse
+
+let applyFrequencyChange frequency change = 
+      frequency + change
+
+let calculateFrequencyChanges startingFrequency changes = 
+    let mutable result = startingFrequency
+    for change in changes do
+         let resultChange = applyFrequencyChange result change
+         result <- resultChange
+    result
+
+[<EntryPoint>]
+let main argv =
+    let startingFrequency = 0
+    let result = calculateFrequencyChanges startingFrequency frequencyChanges
+    
+    printfn "%i" result
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
---
+++ Tests/Day1.fs
@@ -0,0 +1,55 @@
+﻿module Day1
+
+open System
+open NUnit.Framework
+open Day1Program
+
+[<Test>]
+let ``Frequence of 0 changes with +1 results in 1``() =
+    let frequencyStart = 0
+    let frequencyChange = 1
+
+    let expected = 1
+
+    let result = Day1Program.applyFrequencyChange frequencyStart frequencyChange
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``Frequence of 1 changes with -2 results in -1``() =
+    let frequencyStart = 1
+    let frequencyChange = -2
+
+    let expected = -1
+
+    let result = Day1Program.applyFrequencyChange frequencyStart frequencyChange
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``Frequence of -1 changes with +3 results in 2``() =
+    let frequencyStart = -1
+    let frequencyChange = +3
+
+    let expected = 2
+
+    let result = Day1Program.applyFrequencyChange frequencyStart frequencyChange
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``Frequence of 2 changes with +1 results in 3``() =
+    let frequencyStart = 2
+    let frequencyChange = 1
+
+    let expected = 3
+
+    let result = Day1Program.applyFrequencyChange frequencyStart frequencyChange
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``Frequency changes with +1 and -6 results in -1``() =
+    let frequencyStart = 4
+    let frequencyChange = [1; -6]
+
+    let expected = -1
+
+    let result = Day1Program.calculateFrequencyChanges frequencyStart frequencyChange
+    Assert.AreEqual(expected, result)
